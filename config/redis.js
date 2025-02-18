c95624af040be4a98a9d5d27de6a30b63374b23e
Abhishek Kumar <abhishek@tellisim.com>
2025-02-18 22:02:28 +0530

    Refactor docker-compose and redis configuration for improved service health and environment variables

--- config/redis.js
+++ config/redis.js
@@ -1,18 +1,27 @@
 require("dotenv").config();
 const redis = require("redis");
 const { promisify } = require("util");
+//print redius hsot and port
+console.log("redis host", process.env.REDIS_HOST);
+console.log("redis port", process.env.REDIS_PORT);
+
+
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  legacyMode: true,
+ socket: {
+   host: process.env.REDIS_HOST,
+   port:  process.env.REDIS_PORT,
+ },
+ legacyMode: true,
 });
+
 
 redisClient.on('error', (err) => {
-    console.error('Redis Client Error:', err);
+   console.error('Redis Client Error:', err);
 });
 
+
 redisClient.on('connect', () => {
-    console.log('Redis Client Connected');
+   console.log('Redis Client Connected');
 });
 redisClient.connect().catch(console.error);
 const getAsync = promisify(redisClient.get).bind(redisClient);
